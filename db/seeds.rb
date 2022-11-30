5a54231e6f8a7315692e7b9dbdaa9ba227e722ae
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-30 16:48:12 -0700

    commit tree 5bfe44236f26182407cca506917cbe98d614b0b0

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,15 @@ Artist.delete_all
 
 hotline_bling = Song.create(name: 'Hotline Bling')
 thriller = Song.create(name: 'Thriller')
+johns_song = Song.create(name: 'johns song')
 
 drake = Artist.create(name: 'Drake')
 mj = Artist.create(name: 'Michael Jackson')
+john = Artist.create(name: 'jmac')
 
 rap = Genre.create(name: 'Rap')
 pop = Genre.create(name: 'Pop')
+
 
 hotline_bling.artist = drake
 thriller.artist = mj
@@ -19,3 +22,4 @@ mj.songs << thriller
 
 pop.songs << thriller
 rap.songs << hotline_bling
+puts "HELLO"
